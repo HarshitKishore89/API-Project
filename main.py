e8e16926cb69156e956e66254d5ccce5f7846064
HarshitKishore89 <kishorharshit3@gmail.com>
2025-05-15 15:31:26 +0530

    add

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
+def load_data():
+    with open('patients.json', 'r') as f:
+        data = json.load(f)
 
+        return data
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "patient Management Syatem API"}
 
 @app.get("/about")
 def about():
-    return {'message':'my name is harshit kishore '}
+    return {'message':'Fully fuctional API to manage  your patient records.'}
+
+@app.get("/view")
+def view():
+    data = load_data()
+
+    return data
+
